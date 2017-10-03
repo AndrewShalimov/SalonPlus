4870b9893cfa6c442547c7e0bfdfb929db93e898
AndrewShalimov <shalimandr@gmail.com>
2017-10-03 09:39:01 +0300

    add imagick scripts

--- images/put_watermark.bat
+++ images/put_watermark.bat
@@ -1,6 +1,6 @@
 @echo off 
 for %%i in (to_convert/*.jpg) do (^
-   magick composite -watermark 10 -gravity West sp_watermark_logo.png "to_convert/%%i" "watermarked/%%i"^
+   magick composite -watermark 10 -gravity West sp_watermark_logo_word_plus.png "to_convert/%%i" "watermarked/%%i"^
    && echo "composite of %%i successful"^
    && echo "resizing of %%i to 30%% successful"^
    && echo "Successfully deleted %cd%\to_convert\%%i" )
---
+++ images/shrink.bat
@@ -0,0 +1,2 @@
+mogrify -path d:\1\pf\resized -resize 60x60% -quality 60 -format jpg *.jpg
+mogrify -path c:\im\shrinked -resize 40x40% -quality 70 -format jpg *.jpg
